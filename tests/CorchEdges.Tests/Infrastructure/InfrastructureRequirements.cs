98af3f91ed95cf6d0ada371770d0588fdee407af
Shinichi Takano <s-takano@cd6.so-net.ne.jp>
2025-08-06 17:52:38 +0900

    small refactors

---
+++ tests/CorchEdges.Tests/Infrastructure/InfrastructureRequirements.cs
@@ -0,0 +1,14 @@
+﻿namespace CorchEdges.Tests.Infrastructure;
+
+public static class InfrastructureRequirements
+{
+    public const string PostgreSql = "PostgreSQL";
+    public const string AzureServiceBus = "AzureServiceBus";
+    public const string AzureGraphApi = "AzureGraphAPI";
+    
+    // when you uncomment those, you shall add these tests to workflow
+    // 
+    // public const string AzureFunctions = "AzureFunctions";
+    // public const string SharePoint = "SharePoint";
+    // public const string AzureKeyVault = "AzureKeyVault";
+}
